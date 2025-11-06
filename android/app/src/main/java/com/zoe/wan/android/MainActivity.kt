6e9a14ce3b43b1158002b5c614bbc1ce5cda130c
FahuWWWWWWW <3528513306@qq.com>
2025-11-06 10:15:27 +0800

    Manual deployment with correct base href

--- android/app/src/main/java/com/zoe/wan/android/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.zoe.wan.android
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/java/com/zoe/wan/android/WanApp.kt
+++
@@ -1,6 +0,0 @@
-package com.zoe.wan.android
-
-import android.app.Application
-
-class WanApp:Application() {
-}
